6058d3efdc65b34a4f5ccea1d63d012f463ca780
Sumi <johnnoelorano@gmail.com>
2026-02-11 12:11:02 +0800

    google

--- config/google-config.php
+++ config/google-config.php
@@ -1,7 +1,7 @@
 <?php
 // Google OAuth Configuration
 define('GOOGLE_CLIENT_ID', '901503175288-m2pdfhc8tdcsjure87vqnsvnkpchkvjt.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_CLIENT_SECRET', '');
 define('GOOGLE_REDIRECT_URI', 'http://localhost:3000/auth/google-callback.php');
 
 // You can also use the live URL once deployed  
